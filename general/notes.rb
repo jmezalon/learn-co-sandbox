3b9f011d2abf86a3ff4181389c9a801609ef54c0
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-14 19:41:26 +0000

    Done.

---
+++ general/notes.rb
@@ -0,0 +1,21 @@
+# Assignment expression that returns 32
+age = 32
+
+# type in the name
+age
+# and it will look up the expression and display 32
+
+MAMA = :what_does_this_mean
+
+# let's learn about data types
+
+# Ruby has some reserve words like "def" "if" "varchar" or "for"
+
+# data is information and we classified them by types
+
+# the five scalar type are Integer, Float, Boolean, String, Symbol
+
+# any word with the colan infront of it is a symbol
+
+
+# if we want to know what type of class or type a data is we would use the .class after the variable
---
+++ general/notes2.rb
@@ -0,0 +1,38 @@
+y = 6
+x = 8
+y * x
+
+# with devision if we want a precise value we have to use Floats or a combination with Floats and interger
+
+
+# with condition we have the ternary expression
+
+# we use truthy and falsey values as well as comparison operators
+# !! invert truthy or falsey, ! invert true or false, && and || or
+
+rain = TRUE
+
+task = rain ? "bring an umbrella" : "nope"
+
+# we could aslo do
+top = 2
+bottom = 1
+problem_count = 99
+lucky_number = (top > bottom) ? (3 + 1) : (problem_count / 3)
+
+# when it comes to string we could use double or single quotations
+
+# single quotation is useful as well as double specially when it comes
+# to string interpolation 
+
+# we could also use the backslash \ to avoid a string and interpolation
+# looks like this 
+little_woman_name = "joshiny"
+
+"so we could write to #{little_woman_name} like this"
+# if we want to use quotation within the string then we would use the
+# the single quotation
+'the lady said, "what"'
+# if we need to use quotation and interpolation then we would use 
+# the backslash \
+"the #{little_woman_name} said, \"what?\""
