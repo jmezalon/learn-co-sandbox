ce69d3afe039644c6a36d523b59de9940b9f42cd
Jean Max Mezalon <jeanmaxmezalon@pursuit.org>
2019-11-15 12:27:40 +0000

    Done.

---
+++ conversationPart2/notes.rb
@@ -0,0 +1,19 @@
+# expressions always return a value while statement do not return anything
+
+# at its heart, programming is structuring which ecpresions get evalueated and in what order that happens to soleve problems. 
+
+# 3 type of statements: sequence (the order that the code runs), selection (condition for sequence) and repetition (repeat something before moving on)
+
+# the if statement is structured like this:
+
+# if somthing is TRUE
+#   do something here
+# end 
+# we end the if statement with the end key word
+
+age = 23
+ 
+drinking = (age > 21) ? "I'm drinking rum tonight because I'm #{age}." : "let me go study"
+
+puts drinking
+
---
+++ notes.rb
@@ -0,0 +1,7 @@
+
+
+
+
+
+# print prints stuff to the screen and stay on the same line
+# puts   print stuff and goes  to a new line
